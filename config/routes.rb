51d64550556c5ee06dc4f76e198ff329bfd07fb7
hypatiah <danielle.hassid@gmail.com>
2016-09-20 12:09:29 -0700

    Fix bin issues, create Page model, controller and migrations

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  get 'pages/random'
+
+  get 'pages/index'
+
+  get 'pages/show'
+
+  get 'pages/new'
+
+  get 'pages/create'
+
+  get 'pages/edit'
+
+  get 'pages/update'
+
+  get 'pages/delete'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
